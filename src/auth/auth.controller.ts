e51dbc4f3aa21a1b5a81daab7b5405b19d6c5eb9
harshani <ruwanthikapemarathna@gmail.com>
2025-11-03 16:43:02 +0530

    Created register end point

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/user.dto';
+import { UserService } from 'src/user/user.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private userService: UserService) {}
+
+  @Post('register')
+  async registerUser(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
+  }
+}
